9d155f82574648fc40c3b9cd7a7547b06ed1225a
Muhitdinov2008 <147516530+Muhitdinov2008@users.noreply.github.com>
2023-12-21 22:23:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let birinchi = +prompt("1-chi sonni kriting")
+
+let ikkinchi = +prompt("2-chi sonni kriting")
+
+let son = birinchi + 1
+
+let raqam = ikkinchi - 1
+
+console.log("Siz kritgan birinchi son bu (" + birinchi + ") edi lekin sizning raqamingiz ("+ son +") o'zgardi");
+
+console.log("Siz kritgan ikkinchi son bu (" + ikkinchi + ") edi lekin sizning raqamingiz ("+ raqam +") o'zgartirildi");
